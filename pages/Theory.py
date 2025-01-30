537ea5e2b2477d6e191c6d24792a0b927d88eccf
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-01-31 02:00:05 +0530

    Example

--- pages/Theory.py
+++ pages/Theory.py
@@ -14,6 +14,7 @@ $$1 - \phi = 0.382$$
 ### Generic Formula
 Two points x₁ and x₂ are chosen within interval [a, b]:
 $$x_1 = a + (1 - \phi)(b - a)$$
+
 $$x_2 = a + \phi(b - a)$$
 
 ### Steps for Minimization
@@ -22,6 +23,7 @@ $$x_2 = a + \phi(b - a)$$
    - Start with interval [a, b]
    - Compute intermediate points:
    $$x_1 = a + (1 - \phi)(b - a)$$
+
    $$x_2 = a + \phi(b - a)$$
 
 2. **Iterative Update**
@@ -34,16 +36,35 @@ $$x_2 = a + \phi(b - a)$$
    Approximate minimum:
    $$x_{min} = \frac{a + b}{2}$$
 
-### Example
-For f(x) = (x - 2)², interval [1, 3], ε = 0.42:
+### Example for Minimization
 
-Initial points:
+Let's find the minimum of:
+$$f(x) = (x - 2)^2$$
+in the interval [1, 3] with ε = 0.42
+
+#### Step 1: Initial Calculations
+- Initial interval: [1, 3]
+- Calculate x₁ and x₂:
 $$x_1 = 1 + 0.382(3 - 1) = 1.764$$
 $$x_2 = 1 + 0.618(3 - 1) = 2.236$$
+
+#### Step 2: Function Evaluation
+$$f(x_1) = (1.764 - 2)^2 = 0.055$$
+$$f(x_2) = (2.236 - 2)^2 = 0.055$$
+
+#### Step 3: Comparison
+Since f(x₁) = f(x₂) = 0.055:
+- Choose interval [1.764, 3]
+- New interval length = |3 - 1.764| = 1.236
+
+#### Step 4: Termination Check
+Since 1.236 > 0.42, continue iterations...
+
+Final approximate minimum:
+$$x_{min} = 2.000$$
+$$f(x_{min}) = 0.000$$
 ''')
 
-# Add visual separation
-st.markdown("---")
 
 # Additional section for maximization
 st.markdown(r'''
