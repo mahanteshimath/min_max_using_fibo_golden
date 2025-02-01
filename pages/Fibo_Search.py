142d83c54b7c726fffa9bf799d64a5e31b9811b4
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-02-02 01:33:28 +0530

    Update Fibo_Search.py

--- pages/Fibo_Search.py
+++ pages/Fibo_Search.py
@@ -31,7 +31,10 @@ def fibonacci_search(f, a, b, tol=1e-5):
             f1 = f(x1)
         else:
             a = x1 if random.choice([True, False]) else x2
-        n -= 1
+        if n > 1:
+            n -= 1
+        else:
+            break
 
     # Final interval adjustment
     if f1 < f2:
